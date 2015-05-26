606e7180344c83687cf42885472863796d9ad63c
jesusl <jesus@sys.grupomabis.com>
2015-05-26 09:53:41 +0200

    nombre add

--- server/serverroom.py
+++ server/serverroom.py
@@ -3,67 +3,67 @@ import socket, threading, time,sys
 sockets = {}
 
 class cliente(threading.Thread):
-    def __init__(self,sc,clave):
+
+    def __init__(self,sc,nombre):
         threading.Thread.__init__(self)
-        self.fich = open('C:\Users\jesus\Desktop\Compartida\guiserverroomo\service\\service\clienlog.txt','a')
-        self.socketslist()
-        self.clave = clave
+        self.fich = open('C:\Users\Equipo101\Desktop\\2015pro\Serviciowindowsservidorchat\clienlog.txt','a')
+        self.nombre = nombre
         self.sc = sc
-        self.usu = 'usuariocon'
 
     def run(self):
-        self.broadcast('Conectado '+self.usu)
+        global sockets
+        self.broadcast('Conectado '+self.nombre)
         while True:
             try:
                 received = self.sc.recv(1024)
                 a = received.decode('utf-8')
-                print("Msg:"+str(a))
-                self.broadcast(self.usu+str(a))
+                self.log(self.nombre+': '+str(a)+'\n')
+                self.broadcast(self.nombre+': '+str(a))
             except Exception,e:
+                self.fich.write('Excepcion:\n')
                 self.fich.write(str(e))
+                self.fich.write('Fin Excepcion\n')
                 self.fich.flush()
                 break
-        del sockets[str(self.clave)]
-        self.broadcast('Desconectado '+self.usu)
+        del sockets[str(self.nombre)]
+        self.broadcast('Desconectado '+self.nombre)
 
     def broadcast(self,msg):
+        global sockets
         try:
             for usu,sock in sockets.items():
-                sock.send(self.usu+': '+msg)
+                sock.send(msg)
         except Exception,ex:
             self.fich.write(str(ex))
             self.fich.flush()
 
-    def nameforclave(self):
-        for usu,sock in sockets.items():
-            if a == self.clave:
-                pass
-
-    def socketslist(self):
-        self.fich.write('Lista de sockets\n')
-        for i,v in sockets.items():
-            self.fich.write('socket: '+str(v)+' \nclave: '+str(i))
-            self.fich.write('\n')
-            self.fich.flush()
+    def log(self,msg):
+        self.fich.write(str(msg))
+        self.fich.flush()
 
 class server(threading.Thread):
+
     def __init__(self):
-        self.fich = open('C:\Users\jesus\Desktop\Compartida\guiserverroomo\service\\service\log.txt','a')
+        self.fich = open('C:\Users\Equipo101\Desktop\\2015pro\Serviciowindowsservidorchat\log.txt','a')
         threading.Thread.__init__(self)
-        self.clave = 0
 
     def run(self):
+        global sockets
         while True:
             try:
                 s = socket.socket()
                 s.bind(('', 44000))
                 s.listen(5)
                 sc, addr = s.accept()
-                sockets[str(self.clave)] = sc
-                t = cliente(sc,str(self.clave))
+                """obtener el nombre"""
+                recibido = sc.recv(1024)
+                deco = recibido.decode('utf-8')
+                nombre = str(deco)
+                """"""
+                sockets[str(nombre)] = sc
+                t = cliente(sc,str(nombre))
                 t.setDaemon = True
                 t.start()
-                self.clave = self.clave +1
             except Exception,e:
                 self.fich.write(str(e))
                 self.fich.flush()
